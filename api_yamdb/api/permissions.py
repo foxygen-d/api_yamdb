0334d184950379aac485b46d56f3171bf392cce0
gavingreenhorn <gavingreenhorn@gmail.com>
2022-06-27 16:58:28 +0300

    some more stalling

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,14 +6,3 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.id == obj.author.id)
-
-
-class IsModeratorOrReadOnly(permissions.BasePermission):
-
-    ...
-
-
-class IsAdmin(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        return True
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permission_classes
 
-from reviews.models import User
+from reviews.models import User, Review, Comment
 
 
 class UsersViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = ...
     permission_classes = []
+
+
+class ReviewsViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ...
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = ...
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
