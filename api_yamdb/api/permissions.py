d0707ab269c1940928bc6764f860db1d175c6eaa
Anastasia Domnina <foxygen.d@yandex.ru>
2022-07-10 11:41:56 +0300

    Pytest ready

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -16,8 +16,8 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             if (request.user.is_staff or request.user.role == 'admin'
                 or request.user.role == 'moderator'
                 or obj.author == request.user
-                or request.method == 'POST'
-                and request.user.is_authenticated):
+                or request.method == 'POST' and
+                    request.user.is_authenticated):
                 return True
         elif request.method in permissions.SAFE_METHODS:
             return True
